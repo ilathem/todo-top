75f08938bd9fc9c0ac782a3b836639df00e79d00
ilathem <isaiahlathem@gmail.com>
2024-03-16 00:54:43 -0500

    Add html plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -15,4 +16,9 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Todos',
+        }),
+    ],
 };
