a382bd45535a1dadffb9ac4f68e6b63a873221d4
ilathem <isaiahlathem@gmail.com>
2024-03-20 15:04:57 -0500

    Implement readAll

--- src/utils/Storage.js
+++ src/utils/Storage.js
@@ -6,6 +6,21 @@ class LocalStorage {
     constructor() {
         console.log('Creating new local storage object');
     }
+
+    readAll() {
+        const projects = window.localStorage.getItem('projects');
+        if (projects) {
+            return JSON.parse(projects);
+        } else {
+            console.log('no prior data, initializing');
+            this.#initialize();
+            return [];
+        }
+    }
+
+    #initialize() {
+        window.localStorage.setItem('projects', []);
+    }
 }
 
 class Storage {
@@ -23,7 +38,19 @@ class Storage {
         }
     }
 
-    
+    readAll() {
+        const containerOutput = [];
+        for (const container of this.#storageMediums) {
+            containerOutput.push(container.getAll());
+        }
+        const first = containerOutput[0];
+        for (let i = 1; i < containerOutput.length; i++) {
+            if (containerOutput[i] != first) {
+                console.error('all data sources are not equal!');
+            }
+        }
+        return first;
+    }
 }
 
 export default Storage;
