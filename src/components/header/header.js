af75750abba639c2c81ec4d36ee741e3ad299b82
ilathem <isaiahlathem@gmail.com>
2024-03-22 16:21:23 -0500

    Remove test btn

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,4 @@
 import './header.css'
-import todo from '../../utils/Todo';
 
 export default function generateHeader() {
     const title = document.createElement('h1');
@@ -9,9 +8,4 @@ export default function generateHeader() {
     header.classList.add('header');
     header.appendChild(title);
     document.querySelector('body').appendChild(header);
-
-    const testBtn = document.createElement('button');
-    testBtn.innerText = 'test';
-    testBtn.addEventListener('click', () => todo.printProjects())
-    document.querySelector('body').appendChild(testBtn);
 }
